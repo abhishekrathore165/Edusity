7511ae7e91fa89b0d15fbe90f9b2cb28ecd7fe2f
Abhishek Rathore <abhishekrathore468@gmail.com>
2025-04-08 10:51:51 +0530

    Worked on kanu base, pushed to abhi

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -11,18 +11,13 @@ const About = ({setPlayState}) => {
       </div>
       <div className="aboutright">
         <h3>ABOUT UNIVERSITY</h3>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <h2>dhaishdauigfdiagifuagido</h2>
-=======
         <h3>kanu rathore</h3>
->>>>>>> kanu
-=======
         <h2>dhaishdauigfdiagifuagido</h2>
         <h3>kanu rathore ahdbahdhiouhdisaiuduig</h3>
         <h5>start company and do thinks</h5>
+        <h5>new done </h5>
         <h5>now to do things </h5>
->>>>>>> kanu
         <h2>Nurturing Tomorrow's Leader Today</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur neque explicabo alias tempora repellat voluptatum, doloribus reprehenderit iusto hic maxime dolores sapiente tenetur quisquam animi veritatis. Fugiat at cum non!</p>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo quaerat a neque architecto, maxime voluptatum consectetur non ea. Doloremque repellat pariatur cum! Vero alias necessitatibus quis minima amet vel nulla optio voluptates, dolorum illo blanditiis doloremque, veniam nostrum iste nihil!</p>
